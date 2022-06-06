d55e2d09a5b540281330744de00d2a24b7175571
Matt Morelli <mjmorelli0509@gmail.com>
2022-06-06 14:57:36 -0400

    Updates

---
+++ precache-manifest.f1cd94fd988b179d508dcda21509a6ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52779bcfc3380cc5f448c7c393265144",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd2833815c19499f5f93",
+    "url": "/static/css/main.fc09b052.chunk.css"
+  },
+  {
+    "revision": "5deb9d10f2209e9c77e7",
+    "url": "/static/js/2.85fb9bbf.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.85fb9bbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd2833815c19499f5f93",
+    "url": "/static/js/main.493261e3.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "a0ea94c605d7e3ef0af95291df0912da",
+    "url": "/static/media/mmorelli.a0ea94c6.png"
+  },
+  {
+    "revision": "9227118914db291f920f88516acc76fb",
+    "url": "/static/media/resume.92271189.pdf"
+  },
+  {
+    "revision": "05f8d2a3745904dcfa7e8fdfbc9b05ba",
+    "url": "/static/media/trex.05f8d2a3.png"
+  }
+]);
